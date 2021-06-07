4eaa14dfc0cfc48bf43ab8f447b28ca532800eed
Shanaka11 <Shanaka11>
2021-06-07 13:06:37 +0530

    Initial Commit

---
+++ src/theme/theme.js
@@ -0,0 +1,12 @@
+// Material Ui Imports
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    common: {
+      green3: "#77B28C",
+    },
+    primary: "#157A6E",
+    secondary: "#499F68",
+  },
+});
